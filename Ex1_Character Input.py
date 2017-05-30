f1dd8eaff3a6ce4ca07d7503b835ffb162d146a2
RAJAN-GR <rajanrajgor8@gmail.com>
2017-05-30 19:41:39 +0530

    1st Excersice done

--- Ex1_Character Input.py
+++ Ex1_Character Input.py
@@ -7,13 +7,20 @@ import time
 name = input('Enter your name:')
 age = int(input('Enter your age:'))
 
-print('Hello {0} you are {1} year old.'.format(name, age))
+pname = 'Hello {0} you are {1} year old.'.format(name, age)
 
 # 2nd condition To find out the user will be 100 year old in the year of.
 difference = 100 - age
 Year = difference + int(time.strftime('%Y'))
 
 # To tell the user that he/she will be 100 year old in the year of.
-print('And Hey in the year of {0} you will be 100 year old.'.format(Year))
+page = 'And Hey in the year of {0} you will be 100 year old.'.format(Year)
+fprint = pname, page
 
-#This is new change done.push it to github.
+times = int(input('Enter numbers you want to see the massage is repeated on screen:'))
+
+# Message will be printed on user requirement.
+for a in range(0, times):
+    for x in fprint:
+        print(x)
+    print('\n')
